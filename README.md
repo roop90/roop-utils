318f19fe8ffb6c0a2b01d671d1c9c5643f659190
roop90 <ralf.w.1337@gmail.com>
2016-04-16 01:15:09 +0200

    - Updated README - Updated Comments

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # roop-utils
 A collection of usefull Java util classes.
+
+1. Binery Utils
+Info: Provides a Enum for simple and fast convertion between byte arrays and hex, base64 or utf8 String.
+Warning: Does not work under Android.
+Usage:
+		byte[] data = ByteUtil.Base64.getBytes("SGFsbG8gV2VsdA==");
+		String str  = ByteUtil.Base64.getString(data);
+
+2. Hash Utils
+Info: Provides a Enum for simple and fast hashing of a byte array to CRC32, SHA-1, SHA-256 and SHA-512
+Warning: -
+Usage:
+		byte[] data = HashUtil.SHA256.hash(ByteUtil.UTF8.getBytes("Hallo Welt"));
+		byte[] data = HashUtil.SHA256.hash(new File("/test.file");
--- RoopUtilsCore/src/com/roop/utils/hashing/HashUtil.java
+++ RoopUtilsCore/src/com/roop/utils/hashing/HashUtil.java
@@ -1,7 +1,6 @@
 package com.roop.utils.hashing;
 
 import com.roop.utils.Constants;
-import com.roop.utils.Utils;
 import com.roop.utils.binary.ByteUtil;
 import com.roop.utils.binary.IByteUtil;
 
@@ -18,69 +17,69 @@ public enum HashUtil {
 	Sha256{
 		
 		@Override
-		public byte[] doHashing(byte[] in) {
-			return doHashing(in, "SHA-256");
+		public byte[] hash(byte[] in) {
+			return hash(in, "SHA-256");
 		}
 		
 		@Override
-		public byte[] doHashing(InputStream in) throws IOException {
-			return doHashing(in, "SHA-256");
+		public byte[] hash(InputStream in) throws IOException {
+			return hash(in, "SHA-256");
 		}
 
 		@Override
-		public byte[] doHashing(ByteBuffer in) {
-			return doHashing(in, "SHA-256");
+		public byte[] hash(ByteBuffer in) {
+			return hash(in, "SHA-256");
 		}
 
 		@Override
-		public byte[] doHashing(ByteChannel in) throws IOException{
-			return doHashing(in, "SHA-256");
+		public byte[] hash(ByteChannel in) throws IOException{
+			return hash(in, "SHA-256");
 		}
 
 	},
 	Sha512{
 		
 		@Override
-		public byte[] doHashing(byte[] in) {
-			return doHashing(in, "SHA-512");
+		public byte[] hash(byte[] in) {
+			return hash(in, "SHA-512");
 		}
 
 		@Override
-		public byte[] doHashing(InputStream in) throws IOException {
-			return doHashing(in, "SHA-512");
+		public byte[] hash(InputStream in) throws IOException {
+			return hash(in, "SHA-512");
 		}
 
 		@Override
-		public byte[] doHashing(ByteBuffer in) {
-			return doHashing(in, "SHA-512");
+		public byte[] hash(ByteBuffer in) {
+			return hash(in, "SHA-512");
 		}
 
 		@Override
-		public byte[] doHashing(ByteChannel in) throws IOException {
-			return doHashing(in, "SHA-512");
+		public byte[] hash(ByteChannel in) throws IOException {
+			return hash(in, "SHA-512");
 		}
 
 	},
 	Sha1{
 
 		@Override
-		public byte[] doHashing(byte[] in) {
-			return doHashing(in, "SHA");
+		public byte[] hash(byte[] in) {
+			return hash(in, "SHA");
 		}
 
 		@Override
-		public byte[] doHashing(InputStream in) throws IOException {
-			return doHashing(in, "SHA");
+		public byte[] hash(InputStream in) throws IOException {
+			return hash(in, "SHA");
 		}
 
 		@Override
-		public byte[] doHashing(ByteBuffer in) {
-			return doHashing(in, "SHA");
+		public byte[] hash(ByteBuffer in) {
+			return hash(in, "SHA");
 		}
 
 		@Override
-		public byte[] doHashing(ByteChannel in) throws IOException {
-			return doHashing(in, "SHA");
+		public byte[] hash(ByteChannel in) throws IOException {
+			return hash(in, "SHA");
 		}
 
 	},
@@ -96,7 +95,7 @@ public enum HashUtil {
 		}
 
 		@Override
-		public byte[] doHashing(byte[] in) {
+		public byte[] hash(byte[] in) {
 			synchronized (c) {
 				c.reset();
 				c.update(in);
@@ -106,12 +105,23 @@ public enum HashUtil {
 		}
 
 		@Override
-		public byte[] doHashing(InputStream in) throws IOException {
-			return doHashing(Utils.read(in));
+		public byte[] hash(InputStream in) throws IOException {
+//			return hash(Utils.read(in));
+			synchronized (c) {
+				c.reset();
+
+				byte[] buf = new byte[Short.MAX_VALUE];
+				int size;
+				while((size = in.read(buf)) > 0) {
+					c.update(buf, 0, size);
+				}
+
+				return toByte(c.getValue());
+			}
 		}
 
 		@Override
-		public byte[] doHashing(ByteBuffer in) {
+		public byte[] hash(ByteBuffer in) {
 			synchronized (c) {
 				c.reset();
 
@@ -124,7 +134,7 @@ public enum HashUtil {
 		}
 
 		@Override
-		public byte[] doHashing(ByteChannel in) throws IOException {
+		public byte[] hash(ByteChannel in) throws IOException {
 			synchronized (c) {
 				c.reset();
 
@@ -142,20 +152,20 @@ public enum HashUtil {
 	};
 
 	//abstracts
-	public abstract byte[] doHashing(byte[] in);
-	public abstract byte[] doHashing(InputStream in) throws IOException;
-	public abstract byte[] doHashing(ByteBuffer in);
-	public abstract byte[] doHashing(ByteChannel in) throws IOException;
+	public abstract byte[] hash(byte[] in);
+	public abstract byte[] hash(InputStream in) throws IOException;
+	public abstract byte[] hash(ByteBuffer in);
+	public abstract byte[] hash(ByteChannel in) throws IOException;
 	//abstracts end
 
-	protected byte[] doHashing(byte[] data, String instance){
+	protected byte[] hash(byte[] data, String instance){
 		try {
 			return MessageDigest.getInstance(instance).digest(data);
 		} catch (NoSuchAlgorithmException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	protected byte[] doHashing(ByteBuffer bb, String instance){
+	protected byte[] hash(ByteBuffer bb, String instance){
 		try {
 			MessageDigest dig = MessageDigest.getInstance(instance);
 
@@ -166,7 +176,7 @@ public enum HashUtil {
 			throw new RuntimeException(e);
 		}
 	}
-	protected byte[] doHashing(ByteChannel fc, String instance) throws IOException {
+	protected byte[] hash(ByteChannel fc, String instance) throws IOException {
 		try {
 			MessageDigest dig = MessageDigest.getInstance(instance);
 
@@ -183,7 +193,7 @@ public enum HashUtil {
 			throw new RuntimeException(e);
 		}
 	}
-	protected byte[] doHashing(InputStream is, String instance) throws IOException{
+	protected byte[] hash(InputStream is, String instance) throws IOException{
 		try {
 			MessageDigest dig = MessageDigest.getInstance(instance);
 
@@ -200,11 +210,11 @@ public enum HashUtil {
 	}
 
 	/*
-	public byte[] doHashing(VFile file){
+	public byte[] hash(VFile file){
 		try {
 			byte[] out;
 			FileInputStream fis = new FileInputStream(file.fullPath());
-			out = this.doHashing(fis);
+			out = this.hash(fis);
 			fis.close();
 			return out;
 		} catch (FileNotFoundException e) {
@@ -214,41 +224,28 @@ public enum HashUtil {
 	}
 	*/
 
-	public byte[] doHashing(File file){
-		try {
-			byte[] out;
-			FileInputStream fis = new FileInputStream(file);
-			out = this.doHashing(fis);
-			fis.close();
-			return out;
-		} catch (FileNotFoundException e) {
-		} catch (IOException e) {}
-		
-		return null;
+	public byte[] hash(File file) throws IOException{
+		byte[] out;
+		FileInputStream fis = new FileInputStream(file);
+		out = this.hash(fis);
+		fis.close();
+		return out;
 	}
 
-	public byte[] doHashing(FileChannel in){
-		try {
-			return doHashing(in.map(FileChannel.MapMode.READ_ONLY, 0, in.size()));
-		} catch (IOException e) {}
-
-		return null;
+	public byte[] hash(FileChannel in) throws IOException {
+		return hash(in.map(FileChannel.MapMode.READ_ONLY, 0, in.size()));
 	}
 
-	public byte[] doHashing(Path in){
-		try {
-			return doHashing(FileChannel.open(in));
-		} catch (IOException e) {}
-
-		return null;
+	public byte[] hash(Path in) throws IOException {
+		return hash(FileChannel.open(in));
 	}
 	
 	//encoded
-	public String doHashing(File file, IByteUtil encoder){
-		return encoder.getString(this.doHashing(file));
+	public String hash(File file, IByteUtil encoder) throws IOException {
+		return encoder.getString(this.hash(file));
 	}
-	public String doHashing(byte[] data, IByteUtil encoder){
-		return encoder.getString(this.doHashing(data));
+	public String hash(byte[] data, IByteUtil encoder){
+		return encoder.getString(this.hash(data));
 	}
 	//encoded end
 }
